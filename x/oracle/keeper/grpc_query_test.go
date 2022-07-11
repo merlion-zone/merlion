4962ce6d6d592858903b2838b0844bac0cb7d91b
Ho Wader <104507684+howader@users.noreply.github.com>
2022-07-11 10:40:54 +0800

    Add test for oracle genesis, keeper, quries (#20)

--- x/oracle/keeper/grpc_query_test.go
+++ x/oracle/keeper/grpc_query_test.go
@@ -1,21 +1,247 @@
-package keeper_test
+package keeper
 
 import (
-	"testing"
-
+	"bytes"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	testkeeper "github.com/merlion-zone/merlion/testutil/keeper"
 	"github.com/merlion-zone/merlion/x/oracle/types"
 	"github.com/stretchr/testify/require"
+	"sort"
+	"testing"
 )
 
-func TestParamsQuery(t *testing.T) {
-	keeper, ctx := testkeeper.OracleKeeper(t)
-	wctx := sdk.WrapSDKContext(ctx)
-	params := types.DefaultParams()
-	keeper.SetParams(ctx, params)
+// NewQuerier returns an implementation of the oracle QueryServer interface
+// for the provided Keeper.
+func NewQuerier(keeper Keeper) types.QueryServer {
+	return &keeper
+}
 
-	response, err := keeper.Params(wctx, &types.QueryParamsRequest{})
+func TestQueryParams(t *testing.T) {
+	input := CreateTestInput(t)
+	ctx := sdk.WrapSDKContext(input.Ctx)
+
+	querier := NewQuerier(input.OracleKeeper)
+	res, err := querier.Params(ctx, &types.QueryParamsRequest{})
 	require.NoError(t, err)
-	require.Equal(t, &types.QueryParamsResponse{Params: params}, response)
+
+	require.Equal(t, input.OracleKeeper.GetParams(input.Ctx), res.Params)
+}
+
+func TestQueryExchangeRate(t *testing.T) {
+	input := CreateTestInput(t)
+	ctx := sdk.WrapSDKContext(input.Ctx)
+	querier := NewQuerier(input.OracleKeeper)
+
+	rate := sdk.NewDec(1700)
+	input.OracleKeeper.SetExchangeRate(input.Ctx, MicroSDRDenom, rate)
+
+	// empty request
+	_, err := querier.ExchangeRate(ctx, nil)
+	require.Error(t, err)
+
+	// Query to grpc
+	res, err := querier.ExchangeRate(ctx, &types.QueryExchangeRateRequest{
+		Denom: MicroSDRDenom,
+	})
+	require.NoError(t, err)
+	require.Equal(t, rate, res.ExchangeRate)
+}
+
+func TestQueryMissCounter(t *testing.T) {
+	input := CreateTestInput(t)
+	ctx := sdk.WrapSDKContext(input.Ctx)
+	querier := NewQuerier(input.OracleKeeper)
+
+	missCounter := uint64(1)
+	input.OracleKeeper.SetMissCounter(input.Ctx, ValAddrs[0], missCounter)
+
+	// empty request
+	_, err := querier.MissCounter(ctx, nil)
+	require.Error(t, err)
+
+	// Query to grpc
+	res, err := querier.MissCounter(ctx, &types.QueryMissCounterRequest{
+		ValidatorAddr: ValAddrs[0].String(),
+	})
+	require.NoError(t, err)
+	require.Equal(t, missCounter, res.MissCounter)
+}
+
+func TestQueryExchangeRates(t *testing.T) {
+	input := CreateTestInput(t)
+	ctx := sdk.WrapSDKContext(input.Ctx)
+	querier := NewQuerier(input.OracleKeeper)
+
+	rate := sdk.NewDec(1700)
+	input.OracleKeeper.SetExchangeRate(input.Ctx, MicroSDRDenom, rate)
+	input.OracleKeeper.SetExchangeRate(input.Ctx, MicroUSDDenom, rate)
+
+	res, err := querier.ExchangeRates(ctx, &types.QueryExchangeRatesRequest{})
+	require.NoError(t, err)
+
+	require.Equal(t, sdk.DecCoins{
+		sdk.NewDecCoinFromDec(MicroSDRDenom, rate),
+		sdk.NewDecCoinFromDec(MicroUSDDenom, rate),
+	}, res.ExchangeRates)
+}
+
+func TestQueryActives(t *testing.T) {
+	input := CreateTestInput(t)
+	ctx := sdk.WrapSDKContext(input.Ctx)
+	querier := NewQuerier(input.OracleKeeper)
+
+	rate := sdk.NewDec(1700)
+	input.OracleKeeper.SetExchangeRate(input.Ctx, MicroSDRDenom, rate)
+	input.OracleKeeper.SetExchangeRate(input.Ctx, MicroKRWDenom, rate)
+	input.OracleKeeper.SetExchangeRate(input.Ctx, MicroUSDDenom, rate)
+
+	res, err := querier.Actives(ctx, &types.QueryActivesRequest{})
+	require.NoError(t, err)
+
+	targetDenoms := []string{
+		MicroKRWDenom,
+		MicroSDRDenom,
+		MicroUSDDenom,
+	}
+
+	require.Equal(t, targetDenoms, res.Actives)
+}
+
+func TestQueryFeederDelegation(t *testing.T) {
+	input := CreateTestInput(t)
+	ctx := sdk.WrapSDKContext(input.Ctx)
+	querier := NewQuerier(input.OracleKeeper)
+
+	input.OracleKeeper.SetFeederDelegation(input.Ctx, ValAddrs[0], Addrs[1])
+
+	// empty request
+	_, err := querier.FeederDelegation(ctx, nil)
+	require.Error(t, err)
+
+	res, err := querier.FeederDelegation(ctx, &types.QueryFeederDelegationRequest{
+		ValidatorAddr: ValAddrs[0].String(),
+	})
+	require.NoError(t, err)
+
+	require.Equal(t, Addrs[1].String(), res.FeederAddr)
+}
+
+func TestQueryAggregatePrevote(t *testing.T) {
+	input := CreateTestInput(t)
+	ctx := sdk.WrapSDKContext(input.Ctx)
+	querier := NewQuerier(input.OracleKeeper)
+
+	prevote1 := types.NewAggregateExchangeRatePrevote(types.AggregateVoteHash{}, ValAddrs[0], 0)
+	input.OracleKeeper.SetAggregateExchangeRatePrevote(input.Ctx, ValAddrs[0], prevote1)
+	prevote2 := types.NewAggregateExchangeRatePrevote(types.AggregateVoteHash{}, ValAddrs[1], 0)
+	input.OracleKeeper.SetAggregateExchangeRatePrevote(input.Ctx, ValAddrs[1], prevote2)
+
+	// validator 0 address params
+	res, err := querier.AggregatePrevote(ctx, &types.QueryAggregatePrevoteRequest{
+		ValidatorAddr: ValAddrs[0].String(),
+	})
+	require.NoError(t, err)
+	require.Equal(t, prevote1, res.AggregatePrevote)
+
+	// empty request
+	_, err = querier.AggregatePrevote(ctx, nil)
+	require.Error(t, err)
+
+	// validator 1 address params
+	res, err = querier.AggregatePrevote(ctx, &types.QueryAggregatePrevoteRequest{
+		ValidatorAddr: ValAddrs[1].String(),
+	})
+	require.NoError(t, err)
+	require.Equal(t, prevote2, res.AggregatePrevote)
+}
+
+func TestQueryAggregatePrevotes(t *testing.T) {
+	input := CreateTestInput(t)
+	ctx := sdk.WrapSDKContext(input.Ctx)
+	querier := NewQuerier(input.OracleKeeper)
+
+	prevote1 := types.NewAggregateExchangeRatePrevote(types.AggregateVoteHash{}, ValAddrs[0], 0)
+	input.OracleKeeper.SetAggregateExchangeRatePrevote(input.Ctx, ValAddrs[0], prevote1)
+	prevote2 := types.NewAggregateExchangeRatePrevote(types.AggregateVoteHash{}, ValAddrs[1], 0)
+	input.OracleKeeper.SetAggregateExchangeRatePrevote(input.Ctx, ValAddrs[1], prevote2)
+	prevote3 := types.NewAggregateExchangeRatePrevote(types.AggregateVoteHash{}, ValAddrs[2], 0)
+	input.OracleKeeper.SetAggregateExchangeRatePrevote(input.Ctx, ValAddrs[2], prevote3)
+
+	expectedPrevotes := []types.AggregateExchangeRatePrevote{prevote1, prevote2, prevote3}
+	sort.SliceStable(expectedPrevotes, func(i, j int) bool {
+		addr1, _ := sdk.ValAddressFromBech32(expectedPrevotes[i].Voter)
+		addr2, _ := sdk.ValAddressFromBech32(expectedPrevotes[j].Voter)
+		return bytes.Compare(addr1, addr2) == -1
+	})
+
+	res, err := querier.AggregatePrevotes(ctx, &types.QueryAggregatePrevotesRequest{})
+	require.NoError(t, err)
+	require.Equal(t, expectedPrevotes, res.AggregatePrevotes)
+}
+
+func TestQueryAggregateVote(t *testing.T) {
+	input := CreateTestInput(t)
+	ctx := sdk.WrapSDKContext(input.Ctx)
+	querier := NewQuerier(input.OracleKeeper)
+
+	vote1 := types.NewAggregateExchangeRateVote(types.ExchangeRateTuples{{Denom: "", ExchangeRate: sdk.OneDec()}}, ValAddrs[0])
+	input.OracleKeeper.SetAggregateExchangeRateVote(input.Ctx, ValAddrs[0], vote1)
+	vote2 := types.NewAggregateExchangeRateVote(types.ExchangeRateTuples{{Denom: "", ExchangeRate: sdk.OneDec()}}, ValAddrs[1])
+	input.OracleKeeper.SetAggregateExchangeRateVote(input.Ctx, ValAddrs[1], vote2)
+
+	// empty request
+	_, err := querier.AggregateVote(ctx, nil)
+	require.Error(t, err)
+
+	// validator 0 address params
+	res, err := querier.AggregateVote(ctx, &types.QueryAggregateVoteRequest{
+		ValidatorAddr: ValAddrs[0].String(),
+	})
+	require.NoError(t, err)
+	require.Equal(t, vote1, res.AggregateVote)
+
+	// validator 1 address params
+	res, err = querier.AggregateVote(ctx, &types.QueryAggregateVoteRequest{
+		ValidatorAddr: ValAddrs[1].String(),
+	})
+	require.NoError(t, err)
+	require.Equal(t, vote2, res.AggregateVote)
+}
+
+func TestQueryAggregateVotes(t *testing.T) {
+	input := CreateTestInput(t)
+	ctx := sdk.WrapSDKContext(input.Ctx)
+	querier := NewQuerier(input.OracleKeeper)
+
+	vote1 := types.NewAggregateExchangeRateVote(types.ExchangeRateTuples{{Denom: "", ExchangeRate: sdk.OneDec()}}, ValAddrs[0])
+	input.OracleKeeper.SetAggregateExchangeRateVote(input.Ctx, ValAddrs[0], vote1)
+	vote2 := types.NewAggregateExchangeRateVote(types.ExchangeRateTuples{{Denom: "", ExchangeRate: sdk.OneDec()}}, ValAddrs[1])
+	input.OracleKeeper.SetAggregateExchangeRateVote(input.Ctx, ValAddrs[1], vote2)
+	vote3 := types.NewAggregateExchangeRateVote(types.ExchangeRateTuples{{Denom: "", ExchangeRate: sdk.OneDec()}}, ValAddrs[2])
+	input.OracleKeeper.SetAggregateExchangeRateVote(input.Ctx, ValAddrs[2], vote3)
+
+	expectedVotes := []types.AggregateExchangeRateVote{vote1, vote2, vote3}
+	sort.SliceStable(expectedVotes, func(i, j int) bool {
+		addr1, _ := sdk.ValAddressFromBech32(expectedVotes[i].Voter)
+		addr2, _ := sdk.ValAddressFromBech32(expectedVotes[j].Voter)
+		return bytes.Compare(addr1, addr2) == -1
+	})
+
+	res, err := querier.AggregateVotes(ctx, &types.QueryAggregateVotesRequest{})
+	require.NoError(t, err)
+	require.Equal(t, expectedVotes, res.AggregateVotes)
+}
+
+func TestQueryVoteTargets(t *testing.T) {
+	input := CreateTestInput(t)
+	ctx := sdk.WrapSDKContext(input.Ctx)
+	querier := NewQuerier(input.OracleKeeper)
+
+	voteTargets := []string{"denom", "denom2", "denom3"}
+	for _, target := range voteTargets {
+		input.OracleKeeper.SetVoteTarget(input.Ctx, target)
+	}
+
+	res, err := querier.VoteTargets(ctx, &types.QueryVoteTargetsRequest{})
+	require.NoError(t, err)
+	require.Equal(t, voteTargets, res.VoteTargets)
 }
