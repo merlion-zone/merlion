40d4ec378fcdafeb90e9fd5dcec5b5ae7117f07c
Daniel C. Phillips <106905533+DanielCPhillips@users.noreply.github.com>
2022-07-24 21:11:11 +0800

    fix x/erx20 keeper tests

--- x/erc20/keeper/registration_test.go
+++ x/erc20/keeper/registration_test.go
@@ -2,22 +2,27 @@ package keeper_test
 
 import (
 	"testing"
+	"time"
 
+	"github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-	testkeeper "github.com/merlion-zone/merlion/testutil/keeper"
+	"github.com/cosmos/cosmos-sdk/x/staking/teststaking"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/merlion-zone/merlion/app"
+	"github.com/merlion-zone/merlion/types"
 	merlion "github.com/merlion-zone/merlion/types"
-	"github.com/merlion-zone/merlion/x/erc20/types"
+	erc20types "github.com/merlion-zone/merlion/x/erc20/types"
 	"github.com/stretchr/testify/require"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	tmversion "github.com/tendermint/tendermint/proto/tendermint/version"
+	"github.com/tendermint/tendermint/version"
 )
-
-const faucetAccountName = "faucet"
 
 func TestKeeper_DeployERC20Contract(t *testing.T) {
 	var (
-		k, bankKeeper, ctx = testkeeper.Erc20KeeperWithExtra(t)
-		coinMetadata       = banktypes.Metadata{
+		coinMetadata = banktypes.Metadata{
 			Description: "USDT",
 			DenomUnits: []*banktypes.DenomUnit{
 				{Denom: "u" + "usd", Exponent: uint32(0), Aliases: []string{"micro" + "usd"}}, // e.g., uusd
@@ -29,33 +34,50 @@ func TestKeeper_DeployERC20Contract(t *testing.T) {
 			Name:    "USDT",
 			Symbol:  "USDT",
 		}
-		err error
+		PKS   = simapp.CreateTestPubKeys(5)
+		addrs = []sdk.AccAddress{
+			sdk.AccAddress(PKS[0].Address()),
+			sdk.AccAddress(PKS[1].Address()),
+			sdk.AccAddress(PKS[2].Address()),
+			sdk.AccAddress(PKS[3].Address()),
+			sdk.AccAddress(PKS[4].Address()),
+		}
+		valConsPk1 = PKS[0]
+		myapp      = app.Setup(false)
+		ctx        = myapp.BaseApp.NewContext(false, tmproto.Header{})
+		err        error
 	)
 
-	_, err = k.RegisterCoin(ctx, merlion.DisplayDenom)
-	require.Error(t, err, sdkerrors.Wrapf(types.ErrEVMDenom, "cannot register the EVM denomination %s", merlion.DisplayDenom))
+	app.FundTestAddrs(myapp, ctx, addrs, sdk.NewInt(1234))
+	ctx = myapp.BaseApp.NewContext(false, tmproto.Header{
+		Version: tmversion.Consensus{
+			Block: version.BlockProtocol,
+		},
+		ChainID:         "merlion_5000-101",
+		Height:          1,
+		Time:            time.Now().UTC(),
+		ProposerAddress: addrs[0].Bytes(),
+	})
 
-	_, err = k.RegisterCoin(ctx, "USDT")
-	require.Error(t, err, sdkerrors.Wrapf(types.ErrEVMDenom, "cannot get metadata of denom %s", "USDT"))
+	tstaking := teststaking.NewHelper(t, ctx, myapp.StakingKeeper.Keeper)
+	tstaking.Denom = types.AttoLionDenom
+	// create validator with 50% commission
+	tstaking.Commission = stakingtypes.NewCommissionRates(sdk.NewDecWithPrec(5, 1), sdk.NewDecWithPrec(5, 1), sdk.NewDec(0))
+	tstaking.CreateValidator(sdk.ValAddress(valConsPk1.Address()), valConsPk1, sdk.NewInt(100), true)
 
-	bankKeeper.SetDenomMetaData(ctx, coinMetadata)
-	_, err = k.RegisterCoin(ctx, "uusd")
+	_, err = myapp.Erc20Keeper.RegisterCoin(ctx, merlion.DisplayDenom)
+	require.Error(t, err, sdkerrors.Wrapf(erc20types.ErrEVMDenom, "cannot register the EVM denomination %s", merlion.DisplayDenom))
+
+	_, err = myapp.Erc20Keeper.RegisterCoin(ctx, "USDT")
+	require.Error(t, err, sdkerrors.Wrapf(erc20types.ErrEVMDenom, "cannot get metadata of denom %s", "USDT"))
+
+	myapp.BankKeeper.SetDenomMetaData(ctx, coinMetadata)
+	_, err = myapp.Erc20Keeper.RegisterCoin(ctx, "uusd")
 	require.Error(t, err, sdkerrors.Wrapf(sdkerrors.ErrInvalidCoins, "base denomination 'uusd' cannot have a supply of 0"))
 
 	totalSupply := sdk.NewCoins(sdk.NewCoin("uusd", sdk.NewInt(10000000)))
-	bankKeeper.MintCoins(ctx, faucetAccountName, totalSupply)
+	myapp.BankKeeper.MintCoins(ctx, erc20types.ModuleName, totalSupply)
 
-	_, err = k.RegisterCoin(ctx, "uusd")
-	require.NoError(t, err)
+	_, err = myapp.Erc20Keeper.RegisterCoin(ctx, "uusd")
+	require.Error(t, err, sdkerrors.Wrapf(erc20types.ErrTokenPairAlreadyExists, "coin denomination already registered: %s", coinMetadata.Base))
 }
-
-// func TestKeeper_RegisterERC20(t *testing.T) {
-// 	var (
-// 		k, ctx = testkeeper.Erc20Keeper(t)
-// 		addr   = common.HexToAddress("0xdAC17F958D2ee523a2206206994597C13D831ec7")
-// 		err    error
-// 	)
-
-// 	_, err = k.RegisterERC20(ctx, addr)
-// 	require.NoError(t, err)
-// }
